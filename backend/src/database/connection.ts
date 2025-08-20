3e5cf1039bb480683beb7413ce5872405bd481cd
João Gabriel <joaogabrielbochlivio@gmail.com>
2025-08-20 20:35:56 -0300

    adiciona docker compose para subir postgres e kysely como conexao ao db e query builder

---
+++ backend/src/database/connection.ts
@@ -0,0 +1,10 @@
+import { Kysely, PostgresDialect } from 'kysely'
+import { Pool } from 'pg'
+
+const dialect = new PostgresDialect({
+  pool: new Pool({
+    connectionString: process.env.DATABASE_URL,
+  }),
+})
+
+export const db = new Kysely({ dialect })
